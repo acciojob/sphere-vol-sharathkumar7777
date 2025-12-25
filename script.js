ff3cfc34d3b449a13725226054127ba226a0db62
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-25 13:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function volume_sphere() {
     //Write your code here
+
+	const radius = parseFloat(document.getElementById("radius").value);
+      let volume;
+
+      if (isNaN(radius) || radius < 0) {
+        volume = NaN;
+      } else {
+        volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        volume = volume.toFixed(4);
+      }
+
+      document.getElementById("volume").value = volume;
+      return false; // prevent form refresh
   
 } 
 
